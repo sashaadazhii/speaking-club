139589ff880fca517136849e0dc765ce21108e92
sashaadazhii <sashki.sashka@gmail.com>
2023-03-06 16:57:20 +0200

    update

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video10.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-2">
@@ -20,7 +20,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video1.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-3">
@@ -29,7 +29,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video3.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-4">
@@ -38,7 +38,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video4.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-5">
@@ -47,7 +47,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video5.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-6">
@@ -56,7 +56,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video6.mp4"
               ></video>
             </div>
           </div>
@@ -83,7 +83,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video7.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-8">
@@ -92,7 +92,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video8.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-9">
@@ -101,7 +101,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video9.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-10">
@@ -110,7 +110,7 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video12.mp4"
               ></video>
             </div>
             <div class="hero__video-wrap v-11">
@@ -119,8 +119,109 @@
                 muted
                 loop
                 class="hero__video"
-                src="https://goo.by/iufbx"
+                src="https://ebms.s3.ca-central-1.amazonaws.com/video-test/video11.mp4"
               ></video>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="container">
+        <div class="banner"></div>
+        <div class="text-wrap">
+          <h2 class="title">просто вчимо тебе говорити</h2>
+          <p class="text">
+            In the same way that the outer light, that of the sun, illuminates
+            the physical world, the inner light, that of Reason, illuminates the
+            metaphysical world. Sunlight reflects off objects just as Reason’s
+            light reflects off Subjects. Reason is the thread of light that
+            unites the I with world, the particular with the universal, the
+            Subject with the object, the mind with the matter.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="center"><h2 class="title">наші переваги</h2></div>
+
+        <div class="advantages">
+          <div class="advantages__item">
+            <div class="advantages__number">1</div>
+            <div class="advantages__text">фідбек від тічерів</div>
+          </div>
+          <div class="advantages__item">
+            <div class="advantages__number">2</div>
+            <div class="advantages__text">доступ до Netflix</div>
+          </div>
+          <div class="advantages__item">
+            <div class="advantages__number">3</div>
+            <div class="advantages__text">фідбек від тічерів</div>
+          </div>
+          <div class="advantages__item">
+            <div class="advantages__number">4</div>
+            <div class="advantages__text">фідбек від тічерів</div>
+          </div>
+          <div class="advantages__item">
+            <div class="advantages__number">5</div>
+            <div class="advantages__text">фідбек від тічерів</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <h2 class="title">актуальні курси</h2>
+        <div class="cards">
+          <div class="card">
+            <div class="card__left">
+              <div class="card__date">1 червня — 30 серпня</div>
+              <div class="card__info">
+                <div class="title">зно:англійська</div>
+                <div class="text">
+                  підготуємо тебе до ЗНО з англійської мови. часи і всякі інші
+                  приколи і якийсь загальний опис курсу
+                </div>
+              </div>
+              <div class="card__bottom">
+                <div class="card__accent">
+                  6 тижнів — по 5 год 30 хв/тиждень
+                </div>
+                <div class="card__accent">3600 грн</div>
+              </div>
+            </div>
+            <div class="card__right">
+              <div class="card__subtitle">особливості курсу:</div>
+              <div class="card__row">
+                <img src="../assets/images/done.svg" alt="icon done" />
+                <div class="card__text">групові заняття</div>
+              </div>
+              <div class="card__row">
+                <img src="../assets/images/done.svg" alt="icon done" />
+                <div class="card__text">персональний тічер-ментор</div>
+              </div>
+              <div class="card__row">
+                <img src="../assets/images/done.svg" alt="icon done" />
+                <div class="card__text">безкоштовний доступ до Netflix</div>
+              </div>
+              <div class="card__row">
+                <img src="../assets/images/done.svg" alt="icon done" />
+                <div class="card__text">додаткові групові вебінари</div>
+              </div>
+              <div class="card__buttons">
+                <a href="#" class="card__btn"
+                  ><img src="../assets/images/eye.svg" alt="icon" />
+                  детальніше</a
+                >
+                <a href="#" class="card__btn accent"
+                  ><img
+                    src="../assets/images/eye-2.svg"
+                    alt="icon"
+                  />записатись</a
+                >
+              </div>
             </div>
           </div>
         </div>

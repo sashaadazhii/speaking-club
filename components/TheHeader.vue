5acce4e3fcb4f33097ed943ba980f361f3f08ff0
sashaadazhii <sashki.sashka@gmail.com>
2023-03-06 15:30:13 +0200

    first commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="section header">
+    <div class="container">
+      <div class="header__inner">
+        <div class="header__logo-wrap">
+          <img
+            src="../assets/images/logo.svg"
+            alt="Logo Speaking Club"
+            class="header__logo"
+          />
+        </div>
+        <div class="menu">
+          <a href="#" class="menu__link">курси</a>
+          <a href="#" class="menu__link">команда</a>
+          <a href="#" class="menu__link">лондон-тріп</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+<style lang="scss" scoped>
+@import "@/assets/css/variables";
+.header {
+  height: 10vh;
+  padding: 2vh 0;
+  &__inner {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2vh;
+  }
+  &__logo {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    &-wrap {
+      width: 15vw;
+      height: 100%;
+    }
+  }
+}
+.menu {
+  display: flex;
+
+  align-items: center;
+  gap: 1vh;
+
+  &__link {
+    padding: 1vh;
+    @include font(1.25em, $dark, 700, 1);
+    transition: 0.3s ease-in-out;
+
+    &:hover,
+    &:active {
+      color: #ee9b00;
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="page">
+    <TheHeader />
+    <div class="section hero">
+      <div class="container">
+        <div class="hero__inner">
+          <div class="hero__left">
+            <div class="hero__video-wrap v-1">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-2">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-3">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-4">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-5">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-6">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+          </div>
+          <div class="hero__center">
+            <div class="hero__accent">
+              Офіційний партнер Burlington School у Лондоні
+            </div>
+            <h1 class="hero__title">
+              школа англійської, що не sucks but helps you to <span>speak</span>
+            </h1>
+            <p class="hero__text">
+              In the same way that the outer light, that of the sun, illuminates
+              the physical world, the inner light, that of Reason, illuminates
+            </p>
+            <div class="hero__buttons">
+              <a href="#" class="btn">записатись на курс</a>
+              <a href="#" class="btn transparent">тест на визначення рівня</a>
+            </div>
+          </div>
+          <div class="hero__right">
+            <div class="hero__video-wrap v-7">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-8">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-9">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-10">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+            <div class="hero__video-wrap v-11">
+              <video
+                autoplay
+                muted
+                loop
+                class="hero__video"
+                src="https://goo.by/iufbx"
+              ></video>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  mounted() {
+    // let video1 = this.$refs.video1;
+    // video1.play();
+  },
+};
+</script>
